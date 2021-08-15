0c20d2dcab7d444922913477c4b5565989267022
See Woo Lee <dotolation@gmail.com>
2021-08-16 03:33:23 +0900

    refactor kBestAlgoRun.java

--- src/main/java/graphalgos/graphtests/KBestAlgoRun.java
+++ src/main/java/graphalgos/graphtests/KBestAlgoRun.java
@@ -1,9 +1,9 @@
 package graphalgos.graphtests;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
@@ -15,25 +15,19 @@ import graphalgos.DiverseMeasure;
 
 public class KBestAlgoRun extends DemoRun {
 	
-public KBestAlgoRun(Graph<String, DefaultWeightedEdge> g, String source, String target, int k) {
+public KBestAlgoRun(Graph<String, DefaultWeightedEdge> g, String source, String target, int k, String message) {
 
 		
-		System.out.println("---k-best enumeration---");
+		System.out.println(String.format("---k-best enumeration: %s ---", message));
 		
 		startWatch();
 		
 		KShortestPathAlgorithm<String, DefaultWeightedEdge> eppstein = new EppsteinKShortestPath<>(g);
 		List<GraphPath<String, DefaultWeightedEdge>> kPaths = eppstein.getPaths(source, target, k);
 		
-		List<Set<DefaultWeightedEdge>> edgeSets = new ArrayList<>();
-		
-		for(GraphPath<String, DefaultWeightedEdge> path : kPaths) {
-			
-			Set<DefaultWeightedEdge> edgeSet = new HashSet<>(path.getEdgeList());
-			
-			edgeSets.add(edgeSet);
-			
-		}
+		List<Set<DefaultWeightedEdge>> edgeSets = kPaths.stream().map(path -> new HashSet<>(path.getEdgeList())).collect(Collectors.toList());
+		//edgeSets.forEach(edgeSet -> System.out.println(edgeSet));
+
 		
 		stopWatch();
 		DiverseMeasure.compute(edgeSets, g);
