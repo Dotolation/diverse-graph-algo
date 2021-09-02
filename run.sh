d03132f4703f9abbacb228ce16bda088be9a76bb
See Woo Lee <dotolation@gmail.com>
2021-09-02 12:58:49 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Xms4G -Xmx16G -Xss1G -cp "target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar" shell.DiverseGrid 150 150 10
