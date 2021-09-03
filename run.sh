e23e48b65b3b58186a2b7f668d392a319a738746
See Woo Lee <dotolation@gmail.com>
2021-09-04 01:42:50 +0900

    ran wikivote and slashdot datasets

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-java -Xms4G -Xmx16G -Xss1G -cp "target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar" shell.DiverseGrid 150 150 10
+export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar"
+java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.DiverseGrid 250 250 10
+java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.KBestGrid 250 250 10
+java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP wikivote.txt.gz 10 340
+java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP slashdot.txt.gz 10 340
+java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS nyc_dist.gr.gz 10 340
+java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS florida_dist.gr.gz 10 340
