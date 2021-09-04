96e1485232f94a67deea3ff6874664534a2bc83c
See Woo Lee <dotolation@gmail.com>
2021-09-04 18:47:15 +0900

    replaced sh scripts with test.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar"
-java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.DiverseGrid 250 250 10
-java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.KBestGrid 250 250 10
-java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP wikivote.txt.gz 10 340
-java -Xms16G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP slashdot.txt.gz 10 340
-java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS nyc_dist.gr.gz 10 340
-java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS florida_dist.gr.gz 10 340
--- run2.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar"
-java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS nyc_dist.gr.gz 10 340 &
-java -Xms16G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS florida_dist.gr.gz 10 340 &
