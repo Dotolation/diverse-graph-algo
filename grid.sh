8ecb50474e7b84505d05efc2701990aee40dfcd2
See Woo Lee <dotolation@gmail.com>
2021-09-05 21:42:13 +0900

    modify grid.sh

--- grid.sh
+++ grid.sh
@@ -2,3 +2,7 @@ export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrap
 
 #DIMACS
 java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestGrid 200 10 10 50 100
+mv target/test_result.csv target/grid_result.csv
+git add target/grid_result.csv
+git commit -m "Tested the grid graph"
+git push origin
