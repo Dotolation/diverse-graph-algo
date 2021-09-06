e22a99c7ee27e4daa302d030a5488373a8fe4df2
See Woo Lee <dotolation@gmail.com>
2021-09-07 01:34:44 +0900

    add random.sh

---
+++ random.sh
@@ -0,0 +1,24 @@
+export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar"
+
+#Vertex Size  
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 50000 1000000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 100000 1000000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 250000 1000000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 500000 1000000 2020 10 400
+
+#Overall size   
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 10000 100000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 25000 250000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 50000 500000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 200000 2000000 2020 10 400
+
+#K count 
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 30000 400000 2020 5 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 30000 400000 2020 10 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 30000 400000 2020 15 400
+java -Xms32G -Xmx64G -Xmn48G -Xss1G -cp $CLASSPATHS shell.TestRandom 30000 400000 2020 20 400
+
+mv target/test_result.csv target/random_result.csv
+git add target/random_result.csv
+git commit -m "Tested the random graph"
+git push origin
