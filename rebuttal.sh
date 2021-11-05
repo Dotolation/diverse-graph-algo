78f2b4aa858684f5e032e6e21b6fb78a99d3aa60
See Woo Lee <dotolation@gmail.com>
2021-11-05 12:49:38 +0900

    add new commands

---
+++ rebuttal.sh
@@ -0,0 +1,15 @@
+export CLASSPATHS="target/classes:/home/ymmtlab/.m2/repository/org/jgrapht/jgrapht-core/1.5.1/jgrapht-core-1.5.1.jar:/home/ymmtlab/.m2/repository/org/jheaps/jheaps/0.13/jheaps-0.13.jar"
+
+#SNAP
+java -Xss1G -cp $CLASSPATHS shell.TestSNAP wikivote.txt.gz 10 100 2021
+java -Xss1G -cp $CLASSPATHS shell.TestSNAP slashdot.txt.gz 10 100 2021
+java -Xms8G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP twitter_combined.txt.gz 10 100 2021
+
+#DIMACS
+java -Xms8G -Xmx32G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS nyc_dist.gr.gz 10 100 2021
+java -Xms8G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS florida_dist.gr.gz 10 100 2021
+java -Xms8G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestDIMACS co_dist.gr.gz 10 400 2021
+
+#More SNAPS
+#java -Xms8G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP wiki-talk.txt.gz 10 400 2021
+#java -Xms8G -Xmx48G -Xmn16G -Xss1G -cp $CLASSPATHS shell.TestSNAP google.txt.gz 10 400 2021
